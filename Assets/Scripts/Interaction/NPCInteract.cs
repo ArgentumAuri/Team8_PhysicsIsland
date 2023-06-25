f94caba2da6c6b2aa224f60b25eb6e41c2674efc
FractalHenry <legendaryfrosty@gmail.com>
2023-06-25 16:53:14 +0300

    NPC dialogue pattern

--- Assets/Scripts/Interaction/NPCInteract.cs
+++ Assets/Scripts/Interaction/NPCInteract.cs
@@ -1,11 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class NPCInteract : MonoBehaviour
 {
+    bool isMet = false;
+    bool isFailed = false;
+
+    public string GreetingPhrase;
+    public string DisapointmentPhrase;
+    public string[] RegularPhrases;
+    public int repeatFrom = 2;
+    private int currentPhrase = 0;
     public void Dialog()
     {
-        UnityEngine.Debug.Log("hi!");
+        if (!isMet)
+        {
+            isMet = true;
+            //show dialog
+            //text = GreetingPhrase
+            return;
+        }
+        else if (isMet && !isFailed)
+        {
+            //show dialog
+            //if (currentPhrase<RegularPhrases.length)
+            // text = RegularPhrases[currentPhrase]
+            //else 
+            // {
+            // currentPhrase = repeatFrom;
+            // text = RegularPhrases[currentPhrase]
+            // }
+        }
+        else if (isFailed)
+        {
+            //show dialog
+            // text = DisapointmentPhrase
+        }
     }
 }
